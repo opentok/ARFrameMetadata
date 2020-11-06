3e2046748c253e3e17c54a141f019747667e0cd2
Jeronimo Valli <jeronimo.valli@gmail.com>
2020-11-06 11:53:12 -0300

    OPENTOK-41730: removed header

--- ARFrameMetadata/ARFrameMetadata-Bridging-Header.h
+++ ARFrameMetadata/ARFrameMetadata-Bridging-Header.h
@@ -1,10 +1,3 @@
-//
-//  ARFrameMetadata-Bridging-Header.h
-//  ARFrameMetadata
-//
-//  Created by Jerónimo Valli on 9/22/20.
-//  Copyright © 2020 tokbox. All rights reserved.
-//
 
 #ifndef ARFrameMetadata_Bridging_Header_h
 #define ARFrameMetadata_Bridging_Header_h
